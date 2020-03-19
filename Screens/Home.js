ff0407b2caa25b2d98c793035b3d9261a0577850
Rahm <mustafayev7rahman@gmail.com>
2020-03-19 15:59:43 +0400

    simultaneously animated markers (static version)

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,57 +1,88 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
-    StyleSheet,
-    View,
-    TouchableOpacity,
-    Text
+  StyleSheet,
+  View
 } from 'react-native'
-import MapView,{
-    Marker,
+import MapView, {
+  Marker, AnimatedRegion,
 } from 'react-native-maps'
 export default function (props) {
-
-    const [markerCoordinates, setMarkerCoordinates] = useState({
-        latitude: 37.78825,
-        longitude: -122.4324,
+  const [markers, setMarkers] = useState([
+    {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+    {
+      latitude: 37.78826,
+      longitude: -122.4334,
+    },
+  ])
+  const [animateToCoordinate] = useState(markers.map(
+    marker => new AnimatedRegion({
+      latitude: marker.latitude,
+      longitude: marker.longitude,
+      latitudeDelta: 0,
+      longitudeDelta: 0
     })
-    return (<>
-        <View style={styles.body}>
-        <TouchableOpacity onPress={()=>setMarkerCoordinates(prev => ({
-            latitude: prev.latitude+0.001,
-            longitude: prev.longitude+0.001
-        }))} >
-            <Text>Press me</Text>
-        </TouchableOpacity>
-            <MapView
-                style={{flex:1}}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            >
-                <Marker
-                    coordinate = {
-                        markerCoordinates
-                    }
-                />
-            </MapView>
-        </View>
-    </>
-    )
+  ))
+  useEffect(() => {
+    setInterval(() => {
+      setMarkers(prevState => prevState.map(marker => ({
+        latitude: marker.latitude + 0.00005,
+        longitude: marker.longitude + 0.00005
+      })))
+    }, 3500);
+  }, [])
+  useEffect(
+    () => {
+      animateToCoordinate.map(
+        (coordinate, index) => coordinate.timing(
+          {
+            latitude: markers[index].latitude,
+            longitude: markers[index].longitude,
+            duration: 4000
+          }
+        ).start()
+      )
+    }
+    , [markers])
+  return (<>
+    <View style={styles.body}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {
+          animateToCoordinate.map((coordinate, index) =>
+            <Marker.Animated
+              key = {index}
+              coordinate={
+                coordinate
+              }
+            />
+          )
+        }
+      </MapView>
+    </View>
+  </>
+  )
 }
 const styles = StyleSheet.create({
-    body: {
-        display: 'flex',
-        flex: 1,
-    },
-    text: {
-        textAlign: 'center',
-        fontSize: 25,
-        color: 'white',
-        fontWeight: 'bold',
-        marginBottom: 30
-    }
+  body: {
+    display: 'flex',
+    flex: 1,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 25,
+    color: 'white',
+    fontWeight: 'bold',
+    marginBottom: 30
+  }
 }
 )
